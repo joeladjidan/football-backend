5a985d03ed3a32e4c253a91f59003cd073cb1a11
joel Adjidan <joeladjidan@gmail.com>
2025-12-31 02:33:37 +0100

    Ajout des tests unitaire

---
+++ src/main/java/com/testtechnique/football/repository/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Repository adapters and Spring Data repositories.
+ *
+ * This package contains persistence adapters that implement repository
+ * interfaces and translate domain objects to persistence entities.
+ */
+package com.testtechnique.football.repository;
+
